9e1bdedda0edf2a536cd3e9ae8fa112a2b6d9c25
Unknown <Luther.ninja@gmail.com>
2017-09-28 10:55:10 -0600

    Viking engage, and stone throw

--- AdventureGame/Assets/StoryScript.cs
+++ AdventureGame/Assets/StoryScript.cs
@@ -7,7 +7,7 @@ public class StoryScript : MonoBehaviour {
 
 	public Text textObject;
 
-	public enum States {start, bedroom, main_room, outside, look, };
+	public enum States {start, bedroom, main_room, outside, look, woods, viking };
 	public States myState;
 
 	public bool wood = false;
@@ -56,25 +56,53 @@ public class StoryScript : MonoBehaviour {
 	}
 		
 	void State_outside (){
-		textObject.text = "You rush outside and you see vikings making waste of your village." +
+		if (stone == true) {
+			textObject.text = "You rush outside and you see vikings making waste of your village." +
 			"\nOff to your left you see a small clearing in the woods you could use to escape" +
-			"\n you also see a viking standing over a man rasing an axe" +
+			"\nyou also see a viking standing over a man rasing an axe" +
+			"\n\nPress 'E' to escape" +
+			"\nPress 'V' to engage the viking" +
+			"\nPress 'T' to throw your stone for distraction and attempt to save the man";
+		} else if (stone == false) {
+			textObject.text = "You rush outside and you see vikings making waste of your village." +
+			"\nOff to your left you see a small clearing in the woods you could use to escape" +
+			"\nyou also see a viking standing over a man rasing an axe" +
 			"\n\nPress 'E' to escape" +
 			"\nPress 'V' to engage the viking";
 		}
+		if (Input.GetKeyDown (KeyCode.E)) {
+			myState = States.woods;
+		} else if (Input.GetKeyDown (KeyCode.V)) {
+		}
+
+		}
 	
 	void State_look (){
-		if (wood == true) {
+		if (wood == true && stone == false) {
 			textObject.text = "Looking at the room and the unturned furniture you notice a small stone on the floorthat looks unfamiliar" +
-				"\nyou have taken the wood" +
-				"\n\nPress 'S' to pick up stone" +
-			"\nPress 'o' to go outside";
-		} else {
+			"\n\nPress 'S' to pick up stone" +
+			"\nYou have taken the wood" +
+			"\nPress 'o' to go outside" +
+			"\nPress 'R' to go back to the main room";
+		} else if (stone == true && wood == false) {
 			textObject.text = "Looking at the room and the unturned furniture you notice a small stone on the floorthat looks unfamiliar" +
+			"\n\nYou have taken the stone" +
+			"\nPress 'W' to pick up wood" +
+			"\nPress 'o' to go outside" +
+			"\nPress 'R' to go back to the main room";
+		} else if (wood == true && stone == true) {
+			textObject.text = "Looking at the room and the unturned furniture you notice a small stone on the floorthat looks unfamiliar" +
+				"\n\nYou have taken the stone" +
+				"\nYou have taken the wood" +
+				"\nPress 'o' to go outside" +
+				"\nPress 'R' to go back to the main room";
+		} else if (wood == false && stone == false) {
+			textObject.text = "Looking at the room and the unturned furniture you move towards a table and notice a small stone on the floorthat looks unfamiliar" +
 		"\nAmong the flipped wooden furniture you find a sharp piece of wood" +
 		"\n\nPress 'S' to pick up stone" +
 		"\nPress 'W' to pick up the wood" +
-		"\nPress 'o' to go outside";
+		"\nPress 'o' to go outside" +
+		"\nPress 'R' to go back to main room";
 		}
 		if (Input.GetKeyDown (KeyCode.O)) {
 			myState = States.outside;
@@ -82,6 +110,8 @@ public class StoryScript : MonoBehaviour {
 			wood = true;
 		} else if (Input.GetKeyDown (KeyCode.S)) {
 			stone = true;
+		} else if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.main_room;
 		}
 	}
 }

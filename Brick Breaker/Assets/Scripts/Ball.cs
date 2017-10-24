ba52c5fefdcb6e5a1ba5aa2e499ef5024cb488b2
Unknown <Luther.ninja@gmail.com>
2017-10-24 11:18:55 -0600

    BRICK GAME IS COMIN ALONG
    Bricks crack and break, ball flys around

---
+++ Brick Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public GameObject paddle;
+
+	private bool playing = false;
+
+	private Vector3 paddleToBallVector; //distance from ball to paddle
+	private Rigidbody2D rigid;
+
+
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		rigid = this.GetComponent<Rigidbody2D> ();
+		print ("rigid");
+
+	}
+	
+
+	void Update () {
+	
+		if (playing == false) {
+
+		this.transform.position = paddle.transform.position + paddleToBallVector; //the ball will follow the paddles position with the vector we made
+			//if push button 
+			if(Input.GetMouseButtonDown(0)){
+
+
+				rigid.velocity = new Vector2 (4,20);
+				playing = true;
+			}
+
+			//ball should start flying around the map.
+			//playing = true
+			
+		}
+	
+	}
+}
--- Brick Breaker/Assets/Scripts/health.cs
+++ Brick Breaker/Assets/Scripts/health.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Health : MonoBehaviour {
 
 	public int health;
-
+	public Sprite[] picture;
+	private int count = 0;
 
 	void OnCollisionEnter2D (Collision2D collider){
 	
 		health--;
+		count++;
 
+	
 		if (health <= 0) {
 			Destroy (this.gameObject);
 		}
-
+		GetComponent<SpriteRenderer> ().sprite = picture[count];
 
 
 	}

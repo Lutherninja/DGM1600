fe5333102cc65a1e645a0ee77c4fa79bdd8cc5d3
Unknown <Luther.ninja@gmail.com>
2017-11-02 11:19:54 -0600

    Stars, and meteors

--- Spacechad/Assets/Scripts/PlayerController.cs
+++ Spacechad/Assets/Scripts/PlayerController.cs
@@ -10,16 +10,16 @@ using UnityEngine;
 
 		void Update ()
 		{
-			if(Input.GetKey(KeyCode.UpArrow))
+			if(Input.GetKey(KeyCode.W))
 				transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
 
-			if(Input.GetKey(KeyCode.DownArrow))
+			if(Input.GetKey(KeyCode.S))
 				transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime);
 
-			if(Input.GetKey(KeyCode.LeftArrow))
+			if(Input.GetKey(KeyCode.A))
 				transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
 
-			if(Input.GetKey(KeyCode.RightArrow))
+			if(Input.GetKey(KeyCode.D))
 				transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
 	}
 }

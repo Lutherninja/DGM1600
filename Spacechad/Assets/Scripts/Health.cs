ede97ccb4c4879e5b30dc060e6f76b6f0bd25bf2
Unknown <Luther.ninja@gmail.com>
2017-11-28 11:18:54 -0700

    Made explosion effect
    windwakery explosion effect

---
+++ Spacechad/Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public int health;
+	public GameObject explosionEffect;
+		
+
+public void IncrementHealth(int damage) {
+		
+	health <+ damage;
+	if(health <= 0) {
+		Destroy (gameObject);
+		Instantiate (explosionEffect, transform.ImagePosition, Quaternion,identity);
+	   	}
+	}
+}
